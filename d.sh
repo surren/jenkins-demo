d959c0f49f8f22155c5918edfcd81433bcb25c3b
madhu <surren>
2017-01-07 15:05:30 +0000

    JOB E Run

---
+++ d.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+d=30
+if [ $d -gt 35 ]
+then
+echo "D is greater"
+else
+echo "D is less than 35"
+exit 1
+fi
---
+++ e.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+e=30
+if [ $e -gt 35 ]
+then
+echo "E is greater"
+else
+echo "E is less than 35"
+exit 1
+fi
